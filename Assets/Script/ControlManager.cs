f2c47387700d1351aca69e426a5d1369cb54da95
geekdrums <geekdrums@gmail.com>
2017-06-05 00:23:44 +0900

    throttle input for undo/redo

--- Assets/Script/ControlManager.cs
+++ Assets/Script/ControlManager.cs
@@ -1,26 +1,65 @@
-﻿using System.Collections;
+﻿using System;
+using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UniRx;
+using UniRx.Triggers;
 public class ControlManager : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		GameContext.ControlManager = this;
+		SubscribeKeyInput();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	void SubscribeKeyInput()
+	{
+		KeyCode[] throttleKeys = new KeyCode[]
+		{
+			KeyCode.Z,
+			KeyCode.Y,
+		};
+
+		var updateStream = this.UpdateAsObservable();
+
+		foreach( KeyCode key in throttleKeys )
+		{
+			// 最初の入力
+			updateStream.Where(x => Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(key))
+				.Merge(
+			// 押しっぱなしにした時の自動連打
+			updateStream.Where(x => Input.GetKey(KeyCode.LeftControl) && Input.GetKey(key))
+				.Delay(TimeSpan.FromSeconds(GameContext.Config.ArrowStreamDelayTime))
+				.ThrottleFirst(TimeSpan.FromSeconds(GameContext.Config.ArrowStreamIntervalTime))
+				)
+				.TakeUntil(this.UpdateAsObservable().Where(x => Input.GetKeyUp(key)))
+				.RepeatUntilDestroy(this)
+				.Subscribe(_ => OnThrottleInput(key));
+		}
+	}
+
+	void OnThrottleInput(KeyCode key)
+	{
 		if( GameContext.CurrentActionManager != null )
 		{
-			if( Input.GetKeyDown(KeyCode.Z) && Input.GetKey(KeyCode.LeftControl) )
+			switch( key )
 			{
+			case KeyCode.Z:
 				GameContext.CurrentActionManager.Undo();
-			}
-			else if( Input.GetKeyDown(KeyCode.Y) && Input.GetKey(KeyCode.LeftControl) )
-			{
+				break;
+			case KeyCode.Y:
 				GameContext.CurrentActionManager.Redo();
+				break;
 			}
 		}
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
 	}
 }
