77e3aaf9e420380b4d74d22a474bd4f6245463da
geekdrums <geekdrums@gmail.com>
2017-06-06 11:49:55 +0900

    menu bar prototype

---
+++ Assets/Script/UI/MenuButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuButton : Button {
+
+	bool isOpening_ = false;
+	GameObject menuObject_;
+
+	// Use this for initialization
+	protected override void Start () {
+		base.Start();
+		onClick.AddListener(this.OnClick);
+		menuObject_ = transform.FindChild("Menu").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void OnClick()
+	{
+		isOpening_ = !isOpening_;
+		menuObject_.SetActive(isOpening_);
+	}
+}
